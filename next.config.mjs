5e3ead8a3f8819f3b2f0f458a17ffa581f04811a
Devy Salomon <devysalomon@gmail.com>
2025-06-02 12:35:16 -0500

    mas actualizaciones para ver imagenes

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,12 @@ const nextConfig = {
         port: "3000",
         pathname: "/**",
       },
+      {
+        protocol: "http",
+        hostname: "161.132.47.226",
+        port: "3000",
+        pathname: "/**",
+      },
       // If you need to add your backend URL (assuming it's different)
       // {
       //   protocol: 'http',
